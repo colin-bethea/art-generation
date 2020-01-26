ebc09ad830396fd87f7b6c97cf3a55eee699ce9f
Colin Bethea <colinjbethea@gmail.com>
2020-01-26 15:18:55 -0800

    Edit script with new parameters for larger-scale scraping

--- script.py
+++ script.py
@@ -2,6 +2,6 @@ from google_images_download import google_images_download
 
 response = google_images_download.googleimagesdownload()
 
-arguments = {"keywords":"mountains", "format": "png, jpg", "size": "large, medium", "aspect_ratio": "square"}
+arguments = {"keywords":"mountains", "format": "jpg", "size": "large", "aspect_ratio": "square", "limit": 1000}
 paths = response.download(arguments)
 print(paths)
